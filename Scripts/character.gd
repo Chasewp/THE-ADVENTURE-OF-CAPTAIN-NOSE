e0f264acadeaa22f9c1b89d317125cf628a5a86c
Blizzard03 <mariq.rasyid@gmail.com>
2024-05-29 12:34:50 +0700

    add new some code

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,23 +1,56 @@
-#class_name character
-extends CharacterBody2D
+class_name character extends CharacterBody2D
 
-#Exported Variables
+@onready var actor : Node2D = get_parent()
+
+
+#local variable
+#var _is_bound : bool
+#var _min : Vector2
+#var _max : Vector2
+
+#Local Varibale Jump
+var _direction : float
+var _jump_velocity : float
+
+##Local Variables Smims
+#var water_surface_height : float 
+#var is_in_water : bool
+#var is_below_surface : bool
+
+#Exported Variables Locomotions
 @export_category("Locomotion")
 @export var _speed = 8 
 @export var _acceleration : float = 16
 @export var _deceleration : float = 32
 
+#Exported Variable Jump
 @export_category("Jump")
 @export var _jump_height : float = 2.5
 @export var _air_control : float = 0.5 
 @export var _jump_dust : PackedScene
-#local variable
-var _direction : float
-var _jump_velocity : float
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+#Export Variable Spirates
+@export_category("Sprite")
+@export var _is_facing_left : bool
+@export var _sprite_face_left :bool
+@onready var sprite : Sprite2D = $Sprite2D
+
+#Export Variable Swims
+#@export_category("Smim")
+#@export var _desity : float = -0.1
+#@export var _drag : float = 0.5
+
+ #Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var _sprite : Sprite2D = $Sprite2D
+#@onready var _sprite : Sprite2D = $Sprite2D
+#@onready var _character_control = get_parent()
+
+#Export Combat
+#@export_category("Combat")
+#@export_range(1,100) var  _max_health : int = 5
+#@export_range(0,5) var _invicible_duration : float =0
+#@export_range(0,5) var _attack_damage : int = 1
 
 func _ready():
 	_speed *= Global.ppt
@@ -84,4 +117,4 @@ func _spawn_dust(dust : PackedScene):
 	_dust.flip_h = _sprite.flip_h
 	get_parent().add_child(_dust)
 	
-	
+
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,19 @@
-extends Node
+class_name  player extends Node
 
+@export var actor : character
+@export var in_control:= true
 @onready var _character = get_parent()
 
 func _input(event : InputEvent):
-	if event.is_action_pressed("jump"):
-		_character.jump()
-	if event.is_action_released("jump"):
-		_character.stop_jump()
-		
+	if in_control:
+		if event.is_action_pressed("jump"):
+			_character.jump()
+		if event.is_action_released("jump"):
+			_character.stop_jump()
+		if event.is_action_pressed("Control_char 2"):
+			_character.remove_child(self)
+			_character.add_child($MrCraby)
+			in_control = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta:float):
 	_character.run(Input.get_axis("run_left","run_right"))
